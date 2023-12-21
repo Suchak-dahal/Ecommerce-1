db86ca4a0487cef984e77bdc8651c01674da24e7
Suchak Dahal <sharmasupreme111@gmail.com>
2023-12-21 15:38:26 +0545

    Fourth commit

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .views import *
+from django.views.generic import View
+from .models import *
+
 
 # Create your views here.
+class BaseView(View):
+    views = {}
+
+
+class HomeView(BaseView):
+    def get(self, request):
+        self.views['categories'] = Category.objects.all()
+        self.views['sliders'] = Slider.objects.all()
+        self.views['brands'] = Brand.objects.all()
+        self.views['contact_info'] = ContactInfo.objects.all()
+
+        return render(request, 'index.html', self.views)
