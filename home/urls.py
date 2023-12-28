5a854453ab0c1ef445681845d7908386ddcbeab4
Suchak Dahal <sharmasupreme111@gmail.com>
2023-12-28 10:59:31 +0545

    category

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,9 @@ from django.urls import include, path
 
 from .views import *
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('details/<slug>', ProductDetailView.as_view(), name='details'),
+
 ]
 
--- home/views.py
+++ home/views.py
@@ -16,5 +16,22 @@ class HomeView(BaseView):
         self.views['brands'] = Brand.objects.all()
         self.views['ads'] = Ad.objects.all()
         self.views['contact_info'] = ContactInfo.objects.all()
+        self.views['hot_news'] = Product.objects.filter(label='hot')
+        self.views['new_news'] = Product.objects.filter(label='new')
 
         return render(request, 'index.html', self.views)
+
+
+class CategoryView(BaseView):
+
+    def get(self,request,slug):
+        cat_id = Category.objects.get(slug = slug).id
+        self.views['product_category'] = Product.objects.filter(category_id = cat_id)
+        return render(request, 'category.html', self.views)
+
+
+class ProductDetailView(BaseView):
+
+    def get(self, request, slug):
+        self.views['product_details'] = Product.objects.filter(slug = slug)
+        return render(request, 'product-detail.html', self.views)
